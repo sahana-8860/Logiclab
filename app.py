74960c57d849084d190d5a07b015f2df83fd0bde
Sahana <sahanasabariraja8860@gmail.com>
2026-07-04 19:32:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("Blood Donor Finder")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/request')
+def request_blood():
+    return render_template('request.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
